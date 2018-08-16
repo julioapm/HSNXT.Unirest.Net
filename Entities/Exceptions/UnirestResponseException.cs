d39e8c9b77f99ff55a4525bbd480e5e1ad295383
uwx <cadrekucra@gmx.com>
2018-08-16 03:50:41 -0300

    create BaseUnirestResponseException

--- Entities/Exceptions/UnirestResponseException.cs
+++ Entities/Exceptions/UnirestResponseException.cs
@@ -4,13 +4,28 @@ namespace HSNXT.Unirest.Net.Entities.Exceptions
 {
     /// <inheritdoc cref="System.Exception" />
     /// <summary>
+    /// Represents the base type for exceptions when any request completes with a non-success status code, regardless of
+    /// what the target response type is. Useful for disposing and rethrowing exceptions when you don't need any
+    /// information.
+    /// </summary>
+    public abstract class BaseUnirestResponseException : Exception, IDisposable
+    {
+        protected BaseUnirestResponseException(string message) : base(message)
+        {
+        }
+
+        public abstract void Dispose();
+    }
+    
+    /// <inheritdoc cref="BaseUnirestResponseException" />
+    /// <summary>
     /// Represents an exception thrown when a request completes with a non-success status code, as per
     /// <see cref="BaseHttpResponse"/>.<see cref="BaseHttpResponse.IsSuccess"/>.
     ///
     /// <p>An <see cref="UnirestResponseException{T}"/> object should be disposed after being caught.</p>
     /// </summary>
     /// <typeparam name="T">The original target response type, which a failed response needn't conform to</typeparam>
-    public class UnirestResponseException<T> : Exception, IDisposable
+    public class UnirestResponseException<T> : BaseUnirestResponseException
     {
         /// <summary>
         /// The partial, untouched (by the library) response object.
@@ -22,6 +37,6 @@ namespace HSNXT.Unirest.Net.Entities.Exceptions
             PartialResponse = partialResponse;
         }
 
-        public void Dispose() => PartialResponse?.Dispose();
+        public override void Dispose() => PartialResponse?.Dispose();
     }
 }
